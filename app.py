61837275659654f58c09f29940057552e917cedd
Kevin Stradtman <stradtkt22@gmail.com>
2021-01-09 14:38:33 -0500

    Created the models file and some of app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, g
+
+import models
+
+DEBUG=True
+PORT=8000
+HOST='0.0.0.0'
 
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    """Connect to the database before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@app.after_request
+def after_request(response):
+    """Close the database after each request"""
+    g.db.close()
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=DEBUG, host=HOST, port=PORT)
--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 import datetime
 
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
 from peewee import *
 
 DATABASE = SqliteDatabase('social.db')
 
-class User(Model):
+class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
@@ -14,3 +16,15 @@ class User(Model):
     class Meta:
         database = DATABASE
         order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin
+            )
+        except IntegrityError:
+            raise ValueError('User already exists')
