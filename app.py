e3c70e149d2dfe8f60bbb3734263f0fa509d7632
Kevin Stradtman <stradtkt22@gmail.com>
2021-01-10 10:40:24 -0500

    Created the form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, g
+from flask_login import LoginManager
 
 import models
 
@@ -7,6 +8,17 @@ PORT=8000
 HOST='0.0.0.0'
 
 app = Flask(__name__)
+app.secret_key = 'dfhlefnefewlfihewfl.!!dfjdfjdfljdfpei,43!df;dkfd;fkfd;l'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 @app.before_request
 def before_request():
@@ -21,4 +33,11 @@ def after_request(response):
     return response
 
 if __name__ == '__main__':
+    models.initialize()
+    models.User.create_user(
+        name='Kevin',
+        email='stradtkt@gmail.com',
+        password='password',
+        admin=True
+    )
     app.run(debug=DEBUG, host=HOST, port=PORT)
